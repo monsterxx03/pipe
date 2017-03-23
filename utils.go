5b819af2dc58715a8993055ea75204fc24ca6118
Will Xu <will@glowing.com>
2017-03-23 09:50:51 +0000

    Add missing file

---
+++ utils.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"reflect"
+)
+
+func MapCopy(dst, src interface{}) {
+	dv, sv := reflect.ValueOf(dst), reflect.ValueOf(src)
+
+	for _, k := range sv.MapKeys() {
+		dv.SetMapIndex(k, sv.MapIndex(k))
+	}
+}
